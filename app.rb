21dda93ac0d9c5d0abc37c7ece4600a96ce4f01d
Kate Buckner <ingidora@gmail.com>
2015-03-14 13:47:43 -0400

    Added layout-related files

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'pg'
+require 'sinatra'
+require_relative 'lib/deck'
+require_relative 'lib/player'
+require_relative 'lib/card'
+
+configure :development, :test do
+  require 'pry'
+end
+
+get '/' do
+  redirect '/game'
+end
+
+get '/game' do
+  erb :index, locals: {}
+end
