064f73f7fb8c47171680abfad2cd183950499a6a
Corey Psoinos <coreypsoinos@gmail.com>
2015-03-14 15:28:25 -0400

    Create user and generate card hand based on user name

--- app.rb
+++ app.rb
@@ -1,17 +1,30 @@
+require 'pry'
 require 'pg'
 require 'sinatra'
 require_relative 'lib/deck'
 require_relative 'lib/player'
 require_relative 'lib/card'
+require_relative 'lib/hand'
+require_relative 'game.rb'
+
 
 configure :development, :test do
   require 'pry'
 end
 
+use Rack::Session::Cookie
+
 get '/' do
-  redirect '/game'
+  erb :index
 end
 
 get '/game' do
-  erb :index, locals: {}
+  session[:game] ||= Game.setup
+  session[:game].players << Player.new(session[:user], game.white_deck)
+  erb :game, locals: { game: session[:game] }
+end
+
+post '/signin' do
+  session[:user] = params[:user]
+  redirect '/game'
 end
